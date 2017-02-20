a4a9db5d4f3bc0eaa2981e32c13fe88c03917247
Isabella <isabella.loaiza22@gmail.com>
2017-02-20 13:46:03 -0600

    fixed small bug with parameters

--- memories.py
+++ memories.py
@@ -1,3 +1,8 @@
+import itertools
+from functions import talk,think,die,countby,initialize_net
+from random import random,choice
+import numpy as np
+import json
 
 
 '''This scirpt runs simulations that record the memories and then plots them
@@ -9,7 +14,7 @@ def model0_memories(par, ticks):
 	Runs Model 0 for the given set of parameters and the given number of timesteps.
     The difference with the 'Model 0' is that this model records the SCM at ech tick,
     not every 10 ticks. This is desirable here because the data from this model is to be
-    analyzed case by case, hence we only need to run it a few times. 
+    analyzed case by case, hence we only need to run it a few times.
 
 	Parameters
 	----------
@@ -52,14 +57,23 @@ def model0_memories(par, ticks):
 		longScm.append(len(scm))
 	return longScm, individualMem
 
-pvals = [(i * 0.05) for i in range(1,21)]
-rs = [model0([10, i, 0.1, 0.1, 'complete'],10)  for i in pvals]
-a = [r[0] for r in rs]
-b = [r[1] for r in rs]
-f = open(output1[j],'w+')
-json.dump(a,f)
-f.close()
+output1 = ['CN100-E2-scm-all%d.csv' % i for i in range(1,6)]
+output2 = ['CN100-E2-mem-all%d.csv' % i for i in range(1,6)]
 
-f = open(output2[j],'w+')
-json.dump(b,f)
-f.close()
+pvals = [(i * 0.05) for i in range(1,21)]
+
+for j in range(len(output1)):
+                          #  N,   ,p, q,  beta,     gtype
+    rs = [model0_memories([100, i, 0.1, 0.001, 'complete'],20000)  for i in pvals]
+    a = [r[0] for r in rs]
+    b = [r[1] for r in rs]
+    f = open(output1[j],'w+')
+    json.dump(a,f)
+    f.close()
+
+    f = open(output2[j],'w+')
+    json.dump(b,f)
+    f.close()
+
+# 0.1 = 1, 0.5 = 9, 0.8=15
+# memories('CN100-E2-mem-all%d.csv', 'CN100-E2-scm-all%d.csv',1,9,15, 'test0.1', 'test0.5', 'test0.5' )
