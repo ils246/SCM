22aca92b165d1dd7fcc9fd9d6bd8c6fd9faf8396
Isabella <isabella.loaiza22@gmail.com>
2017-02-22 12:03:53 -0600

    Changed filenames to run different experiment

--- memories.py
+++ memories.py
@@ -9,13 +9,12 @@ import json
 '''This scirpt runs simulations that record the memories and then plots them
         to show the gap between individual memory and collective memory'''
 
-output1 = ['CN100-E2-scm-all%d.csv' % i for i in range(1,6)]
-output2 = ['CN100-E2-mem-all%d.csv' % i for i in range(1,6)]
-
+output1 = ['CN100-E4-scm-all%d.csv' % i for i in range(1,3)]
+output2 = ['CN100-E4-mem-all%d.csv' % i for i in range(1,3)]
 pvals = [(i * 0.05) for i in range(1,21)]
 
 for j in range(len(output1)):
-	#  N,   ,p, q,  beta,     gtype
+	                     #  N,   ,p, q,  beta,     gtype
     rs = [model0_memories([100, i, 0.1, 0.001, 'complete'],20000)  for i in pvals]
     a = [r[0] for r in rs]
     b = [r[1] for r in rs]
@@ -26,6 +25,3 @@ for j in range(len(output1)):
     f = open(output2[j],'w+')
     json.dump(b,f)
     f.close()
-
-# 0.1 = 1, 0.5 = 9, 0.8=15
-# memories('CN100-E2-mem-all%d.csv', 'CN100-E2-scm-all%d.csv',1,9,15, 'test0.1', 'test0.5', 'test0.5' )
