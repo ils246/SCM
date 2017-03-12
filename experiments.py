785e1addff8316d95048ce494c5450c22e5d8ec5
Isabella <isabella.loaiza22@gmail.com>
2017-03-12 18:24:03 -0500

    Added code to run experiments on model1

--- experiments.py
+++ experiments.py
@@ -7,17 +7,44 @@ from joblib import Parallel, delayed
 from multiprocessing import cpu_count
 n_cores = cpu_count()
 
+ticks = 20000
 
 #           Model 0 experiments
 #-----------------------------------------
-#
-#                 N,  q,  beta,   gtype
-N, q,beta,net =[100, 0.001, 0.1,'random']
-ticks = 20000
 
-#Simulations that do record the memories
-output1 = ['RN100-E34-scm%d.csv' % i for i in range(1,11)]
-output2 = ['RN100-E34-mem%d.csv' % i for i in range(1,11)]
+#                 N,  q,  beta,   gtype
+N, q,beta,net =[800, 0.005, 0.005,'complete']
+output1 = ['CN800-E70-scm%d.csv' % i for i in range(10,101)]
+pvals = [(i * 0.05) for i in range(1,21)]
+
+print "Splitting into",n_cores
+for j in range(len(output1)):
+    rs = Parallel(n_jobs=n_cores)(delayed(model0)
+    ([N, i, q, beta, net],ticks) for i in pvals)
+    a = [r[0] for r in rs]
+    f = open(output1[j],'w+')
+    # So this will give me the list per p of length ticks
+    json.dump(a,f)
+    f.close()
+
+N, q,beta,net =[900, 0.005, 0.005,'complete']
+output1 = ['CN900-E71-scm%d.csv' % i for i in range(15,101)]
+pvals = [(i * 0.05) for i in range(1,21)]
+
+print "Splitting into",n_cores
+for j in range(len(output1)):
+    rs = Parallel(n_jobs=n_cores)(delayed(model0)
+    ([N, i, q, beta, net],ticks) for i in pvals)
+    a = [r[0] for r in rs]
+    f = open(output1[j],'w+')
+    # So this will give me the list per p of
+    # length ticks
+    json.dump(a,f)
+    f.close()
+
+
+N, q,beta,net =[1000, 0.005, 0.005,'complete']
+output1 = ['CN1000-E72-scm%d.csv' % i for i in range(10,101)]
 pvals = [(i * 0.05) for i in range(1,21)]
 
 print "Splitting into",n_cores
@@ -26,71 +53,43 @@ for j in range(len(output1)):
     ([N, i, q, beta, net],ticks) for i in pvals)
     # rs = [model0([10, i, 0.1, 0.1, 'complete'],10)  for i in pvals]
     a = [r[0] for r in rs]
-    b = [r[1] for r in rs]
+    # b = [r[1] for r in rs]
     f = open(output1[j],'w+')
     # So this will give me the list per p of
     # length ticks
     json.dump(a,f)
     f.close()
 
-    f = open(output2[j],'w+')
-    # array that is divided by p, then by tick and the items
-    # inside are the memories of each agent.
-    json.dump(b,f)
-    f.close()
+    # f = open(output2[j],'w+')
+    # # array that is divided by p, then by tick and the items
+    # # inside are the memories of each agent.
+    # json.dump(b,f)
+    # f.close()
 
 
 # Simulations that don't record the memories
-output3 = ['RN100-E34-scm%d.csv' % i for i in range(11,101)]
-for k in range(len(output3)):
-        results = Parallel(n_jobs=n_cores)(delayed(model0)
-        ([N, i, q, beta, net],ticks) for i in pvals)
-        c = [r[0] for r in results]
-        p = open(output3[k],'w+')
-        json.dump(c,p)
-        p.close()
+# output3 = ['RN100-E34-scm%d.csv' % i for i in range(11,101)]
+# for k in range(len(output3)):
+#         results = Parallel(n_jobs=n_cores)(delayed(model0)
+#         ([N, i, q, beta, net],ticks) for i in pvals)
+#         c = [r[0] for r in results]
+#         p = open(output3[k],'w+')
+#         json.dump(c,p)
+#         p.close()
+
 
 
 #           Model 1 experiments
 #-----------------------------------------
 
-# N,p, q, beta,net =[100, 0.2, 0.001, 0.01, 'complete']
-# ticks = 20000
-# pvals = [p]*N
-# for i in range(10):
-#     item = np.random.randint(N)
-#     pvals[item]=0.1
+# N, pStar, pHubs, q, beta, net =[100, 0.25, 0.9, 0.01, 0.001,'scale-free']
+# top=10
+# pvals=[pStar]*100
 #
-#
-# #Simulations that do record the memories
-# output1 = ['CN100-E24-scm%d.csv' % i for i in range(1,10)]
-# output2 = ['CN100-E24-mem%d.csv' % i for i in range(1,10)]
-#
-# print "Splitting into",n_cores
-# for j in range(len(output1)):
-#     rs = Parallel(n_jobs=n_cores)(delayed(model1)
-#     ([N, i, q, beta, net],ticks) for i in [pvals]) # supongo que para que cambie entonces le pones shuffle antes del pval
-#     # rs = [model0([10, i, 0.1, 0.1, 'complete'],10)  for i in pvals]
-#     a = [r[0] for r in rs]
-#     b = [r[1] for r in rs]
-#     f = open(output1[j],'w+')
-#     # So this will give me the list per p of
-#     # length ticks
-#     json.dump(a,f)
-#     f.close()
-#
-#     f = open(output2[j],'w+')
-#     # array that is divided by p, then by tick and the items
-#     # inside are the memories of each agent.
-#     json.dump(b,f)
-#     f.close()
-#
-# # # Simulations that don't record the memories
-# output3 = ['CN100-E25-scm%d.csv' % i for i in range(11,101)]
-# for k in range(len(output3)):
-#         results = Parallel(n_jobs=n_cores)(delayed(model1)
-#         ([N, i, q, beta, net],ticks) for i in [pvals])
-#         c = [r[0] for r in results]
-#         p = open(output3[k],'w+')
-#         json.dump(c,p)
-#         p.close()
+# print ("Splitting into",n_cores)
+# rs = Parallel(n_jobs=n_cores)(delayed(model1)
+# ([N, i, pHubs, q, beta, net],ticks,top) for i in pvals)
+# a = [r[0] for r in rs]
+# f = open('SN100-E57-scm.csv','w+')
+# json.dump(a,f)
+# f.close()
