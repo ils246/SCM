3fd6c07ac8544b35c63968481e4f9568244c4a91
Isabella <isabella.loaiza22@gmail.com>
2017-03-21 17:37:36 -0500

    Deleted model0_old

--- models.py
+++ models.py
@@ -98,7 +98,6 @@ def model0(par, ticks,save_every=10,save_individual=False):
 	longScm, individualMem = _master((N,p,q,beta), ticks,G=G, save_every=save_every,save_individual=save_individual)
 	return longScm, individualMem
 
-
 def model1(par, ticks, save_every=10,save_individual=False):
 	'''
 	Runs Model 1 for the given set of parameters and the given number of timesteps.
@@ -165,56 +164,6 @@ def model0_old(par, ticks,save_every=10):
 		for agent in np.random.randint(0,N,N):
 			myp = random()
 			if myp < p:
-				memories = talk(agent,memories,G=G)
-			else:
-				myq = random()
-				if myq < q:
-					memories,idea_tick = think(agent,memories,idea_tick)
-			b = random()
-			if b < beta:
-				memories = die(agent,memories)
-		tempMem = [len(mem) for mem in memories]
-		individualMem.append(tempMem)
-		if save_every == 0 or tick % save_every == 0:
-			scm = set(itertools.chain.from_iterable(memories))
-			longScm.append(len(scm))
-	return longScm, individualMem
-
-
-def model1_old(par, ticks, top, save_every=10):
-	'''
-	THIS FUNCTION WILL BE DELETED SOON
-	Runs Model 1 for the given set of parameters and the given number of timesteps.
-
-	Parameters
-	----------
-	par : tuple
-		Parameters of the simulation.
-		Parameters p,q, and beta can be iterables of size N or floats.
-		N,p,q,beta,gtype
-	ticks : int
-		Number of ticks to run the simulation for.
-
-	Returns
-	-------
-	longScm : list
-		Size of Social Collective Memory as a function of time, recorded every 10 ticks.
-	individualMem : list
-		List of lists.
-		It contains the lengths of the individual memories as a function of time.
-	'''
-	N,pStar,pHubs,q,beta,gtype = par
-	G = initialize_net(gtype,N)
-	ps = set_hubs(G,N,pStar,pHubs,top)
-	memories = [set() for i in range(N)]
-	longScm, recordMem, individualMem = [[] for a in range(3)]
-	scm =set()
-	idea_tick=0
-
-	for tick in range(ticks):
-		for agent in np.random.randint(0,N,N):
-			myp = random()
-			if myp < ps[agent]:
 				memories = talk(agent,memories,G=G)
 			else:
 				myq = random()
